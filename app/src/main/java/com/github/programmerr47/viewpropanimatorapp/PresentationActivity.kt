c0bc042504a41f4e4a2edad30d1772f510c991a0
programmerr47 <m.spitsin@rambler-co.ru>
2017-03-19 20:58:44 +0300

    Next case sample hide and show animations that we use in many projects

--- app/src/main/java/com/github/programmerr47/viewpropanimatorapp/PresentationActivity.kt
+++ app/src/main/java/com/github/programmerr47/viewpropanimatorapp/PresentationActivity.kt
@@ -1,8 +1,12 @@
 package com.github.programmerr47.viewpropanimatorapp
 
-import android.support.v7.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewPropertyAnimator
 import kotlinx.android.synthetic.main.activity_presenation.*
 
 const val ANIM_DURATION = 2000L
@@ -16,5 +20,36 @@ class PresentationActivity : AppCompatActivity() {
     }
 }
 
-fun View.fadeOut() = this.animate().alpha(0f).start()
-fun View.fadeIn() = this.animate().alpha(1f).setDuration(ANIM_DURATION).start()
+fun View.fadeOut() {
+    if (this.visibility != VISIBLE || this.alpha == 0f) {
+        this.visibility = GONE
+        return
+    }
+
+    this.animate()
+            .alpha(0f)
+            .setDuration(ANIM_DURATION)
+            .setEndListener { this.visibility = GONE }
+            .start()
+}
+
+fun View.fadeIn() {
+    if (this.visibility == VISIBLE && this.alpha == 1f) {
+        return
+    }
+
+    if (this.visibility != VISIBLE) {
+        this.visibility = VISIBLE
+        this.alpha = 0f
+    }
+
+    this.animate().alpha(1f).setDuration(ANIM_DURATION).start()
+}
+
+fun ViewPropertyAnimator.setEndListener(listener: () -> Unit) =
+        this.setListener(object : android.animation.AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                listener.invoke()
+            }
+        })
