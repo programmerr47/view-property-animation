f7def744a5f57f5e145c4a11ddb57f2d12094a02
programmerr47 <m.spitsin@rambler-co.ru>
2017-03-19 22:38:07 +0300

    Last case with complex animation that consists of simple anims

--- app/src/main/java/com/github/programmerr47/viewpropanimatorapp/PresentationActivity.kt
+++ app/src/main/java/com/github/programmerr47/viewpropanimatorapp/PresentationActivity.kt
@@ -9,18 +9,22 @@ import android.view.View.VISIBLE
 import android.view.ViewPropertyAnimator
 import kotlinx.android.synthetic.main.activity_presenation.*
 
-const val ANIM_DURATION = 2000L
+const val ANIM_DURATION = 300L
+const val SHOW_DURATION = 2000L
 
 class PresentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_presenation)
-        b_hide.setOnClickListener { _ -> iv_image.fadeOut() }
-        b_show.setOnClickListener { _ -> iv_image.fadeIn() }
+        b_hide.setOnClickListener { iv_image.fadeOut() }
+        b_show_hide.setOnClickListener { iv_image.fadeInOut(SHOW_DURATION) }
+        b_show.setOnClickListener { iv_image.fadeIn() }
     }
 }
 
-fun View.fadeOut() {
+fun View.fadeInOut(showDuration: Long) = fadeIn { fadeOut(showDuration) }
+
+fun View.fadeOut(delay: Long = 0) {
     if (this.visibility != VISIBLE || this.alpha == 0f) {
         this.visibility = GONE
         return
@@ -28,13 +32,15 @@ fun View.fadeOut() {
 
     this.animate()
             .alpha(0f)
+            .setStartDelay(delay)
             .setDuration(ANIM_DURATION)
             .setEndListener { this.visibility = GONE }
             .start()
 }
 
-fun View.fadeIn() {
+fun View.fadeIn(endAction: () -> Unit = {}) {
     if (this.visibility == VISIBLE && this.alpha == 1f) {
+        endAction.invoke()
         return
     }
 
@@ -43,7 +49,11 @@ fun View.fadeIn() {
         this.alpha = 0f
     }
 
-    this.animate().alpha(1f).setDuration(ANIM_DURATION).start()
+    this.animate()
+            .alpha(1f)
+            .setDuration(ANIM_DURATION)
+            .setEndListener(endAction)
+            .start()
 }
 
 fun ViewPropertyAnimator.setEndListener(listener: () -> Unit) =
