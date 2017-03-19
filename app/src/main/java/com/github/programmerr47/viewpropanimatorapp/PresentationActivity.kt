8cda8ea7e0766ef73abe0356e09e1023e18404e6
programmerr47 <m.spitsin@rambler-co.ru>
2017-03-19 13:08:24 +0300

    Moved project to kotlin

---
+++ app/src/main/java/com/github/programmerr47/viewpropanimatorapp/PresentationActivity.kt
@@ -0,0 +1,12 @@
+package com.github.programmerr47.viewpropanimatorapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PresentationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_presenation)
+    }
+}
